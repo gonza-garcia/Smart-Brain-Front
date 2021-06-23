fda77a3964f82c007c317a99140442dd9c0692e0
Gonza García <gcostoya02@gmail.com>
2021-06-23 16:39:51 -0300

    big update to react 17

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,109 +4,115 @@ import { get_from_server } from '../../functions/functions.js';
 const messageRef = React.createRef();
 
 class SignIn extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      message: '',
-      isLoading: false
-    }
-  }
-
-  onEmailChange = event => {
-    this.setState({email: event.target.value})
-  }
-
-  onPasswordChange = event => {
-    this.setState({password: event.target.value})
-  }
-
-  onSubmitSignIn = async event => {
-    event.preventDefault();
-
-    this.setState({ isLoading: true });
-
-    try 
-    {
-        const user = await get_from_server('/signin', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              email: this.state.email,
-              password: this.state.password
-            })
-        });
-        
-        if (user.id) {
-          this.props.loadUser(user);
-          this.props.changeToRoute('Home');
+    constructor(props){
+        super(props);
+        this.state = {
+          email: '',
+          password: '',
+          message: '',
+          isLoading: false
         }
     }
-    catch (error) {
-        const message = (typeof error === 'string') ? error : 'Wrong credential. Please try again';
 
-        this.setState({ isLoading: false });
-        this.setState({ message: message}, () => {
-            messageRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
-        });
-        setTimeout(() => {this.setState({ message: '' })}, 4000);
+    timeoutId = null;
+
+    onEmailChange = event => {
+        this.setState({email: event.target.value})
     }
-  }
+
+    onPasswordChange = event => {
+        this.setState({password: event.target.value})
+    }
+
+    onSubmitSignIn = async event => {
+        event.preventDefault();
+
+        this.setState({ isLoading: true });
+
+        try 
+        {
+            const user = await get_from_server('/signin', {
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                  email: this.state.email,
+                  password: this.state.password
+                })
+            });
+            
+            if (user.id) {
+              this.props.loadUser(user);
+              this.props.changeToRoute('Home');
+            }
+        }
+        catch (error) {
+            const message = (typeof error === 'string') ? error : 'Wrong credentials. Please try again.';
+
+            this.setState({ isLoading: false });
+            this.setState({ message: message}, () => {
+                messageRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
+            });
+            this.timeoutId = setTimeout(() => {this.setState({ message: '' })}, 4000);
+        }
+    }
 
 
-  render() {
-    const {changeToRoute} = this.props;
-    return (
-      <article className="br3 ba b--black-10 mv4 shadow-5 center" style={{maxWidth: '20rem'}}>
-          <form className="pa4 black-80" style={{position:'relative'}}>
-              <div className="measure">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                  <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                  <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                    <input
-                      className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                      type="email"
-                      name="email-address"
-                      id="email-address"
-                      autoComplete='email'
-                      onChange={this.onEmailChange}
+    componentWillUnmount() {
+        clearInterval(this.timeoutId);
+    }
+
+    render() {
+        const {changeToRoute} = this.props;
+        return (
+          <article className="br3 ba b--black-10 mv4 shadow-5 center" style={{maxWidth: '20rem'}}>
+              <form className="pa4 black-80" style={{position:'relative'}}>
+                  <div className="measure">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                      <div className="mt3">
+                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                        <input
+                          className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                          type="email"
+                          name="email-address"
+                          id="email-address"
+                          autoComplete='email'
+                          onChange={this.onEmailChange}
+                          />
+                      </div>
+                      <div className="mv3">
+                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                        <input
+                          className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                          type="password"
+                          name="password"
+                          id="password"
+                          autoComplete='current-password'
+                          onChange={this.onPasswordChange}
+                        />
+                      </div>
+                    </fieldset>
+                    <div className="">
+                      <input
+                        onClick={this.onSubmitSignIn}
+                        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                        type="submit"
+                        value={!this.state.isLoading ? 'Sign In' : 'Please wait...'}
                       />
+                    </div>
+                    <div className="lh-copy mt3">
+                      <p
+                        onClick={() => changeToRoute('Register')}
+                        className="f6 link dim black db pointer">...or Register</p>
+                    </div>
+                    <p
+                      style={{fontSize: '.875rem', color: '#ffbb00', position:'absolute', bottom:0, right:0, left:0, marginBottom:0}}
+                      ref={messageRef}>{this.state.message}</p>
                   </div>
-                  <div className="mv3">
-                    <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                    <input
-                      className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                      type="password"
-                      name="password"
-                      id="password"
-                      autoComplete='current-password'
-                      onChange={this.onPasswordChange}
-                    />
-                  </div>
-                </fieldset>
-                <div className="">
-                  <input
-                    onClick={this.onSubmitSignIn}
-                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                    type="submit"
-                    value={!this.state.isLoading ? 'Sign In' : 'Please wait...'}
-                  />
-                </div>
-                <div className="lh-copy mt3">
-                  <p
-                    onClick={() => changeToRoute('Register')}
-                    className="f6 link dim black db pointer">...or Register</p>
-                </div>
-                <p
-                  style={{fontSize: '.875rem', color: '#ffbb00', position:'absolute', bottom:0, right:0, left:0, marginBottom:0}}
-                  ref={messageRef}>{this.state.message}</p>
-              </div>
-          </form>
-      </article>
-    );
-  }
+              </form>
+          </article>
+        );
+    }
 }
 
 export default SignIn;
--- src/containers/App.js
+++ src/containers/App.js
@@ -12,15 +12,15 @@ import { get_from_server, loadImage } from '../functions/functions.js';
 
 
 const particlesOptions = {
-  particles: {
-    number: {
-      value: 80,
-      density: {
-        enable: true,
-        value_area: 800,
-      }
+    particles: {
+        number: {
+        value: 80,
+        density: {
+            enable: true,
+            value_area: 800,
+        }
+        }
     }
-  }
 }
 
 
@@ -33,11 +33,11 @@ const initialState = {
     message: '',
     isLoading: false,
     user: {
-      id:       '',
-      name:     '',
-      email:    '',
-      entries:  0,
-      joined:   '',
+        id:       '',
+        name:     '',
+        email:    '',
+        entries:  0,
+        joined:   '',
     }
 }
 // const initialState = {
@@ -49,9 +49,9 @@ const initialState = {
 //     message: '',
 //     isLoading: false,
 //     user: {
-//       id:       1,
+//       id:       101,
 //       name:     "gonza",
-//       email:    "gonzalo@gmail.com",
+//       email:    "gonzalo@gonzalo.com",
 //       entries:  6,
 //       joined:   "2019-11-29T19:34:51.218Z",
 //     }
@@ -62,197 +62,198 @@ const imageRef = React.createRef();
 const messageRef = React.createRef();
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
-  }
+    constructor() {
+        super();
+        this.state = initialState;
+    }
 
-  loadUser = user => {
-    this.setState({
-      isSignedIn: true,
-      user: {
-        id:       user.id,
-        name:     user.name,
-        email:    user.email,
-        entries:  user.entries,
-        joined:   user.joined,
-      }
-    }, () => {})
-  }
-
-  calculateFacesLocations = (clarifai_regions) => {
-      const width = Number(imageRef.current.width);
-      const height = Number(imageRef.current.height);
-
-      const boxes = clarifai_regions.map(region => {
-          let clarifai_box = region.region_info.bounding_box;
-          return {
-            leftCol:    width   * clarifai_box.left_col,
-            topRow:     height  * clarifai_box.top_row,
-            rightCol:   width   - (width  * clarifai_box.right_col),
-            bottomRow:  height  - (height * clarifai_box.bottom_row),
-          }
-      })
-
-      return boxes;
-  }
-
-  displayFaceBoxes = (boxes) => {
-    this.setState({ boxes: boxes });
-  }
-
-  onInputChange = (event) => {
-      this.setState({ imageUrl: event.target.value, imageSourceIsLocal: false, boxes: [] });
-  }
-  
-  onImageUpload = (event) => {
-
-      const files = event.target.files[0];
-
-      let fileReader = new FileReader();
-
-      fileReader.onloadend = async () => {
-          this.setState({ imageUrl: fileReader.result, imageSourceIsLocal: true, boxes: [] });
-      }
-
-      if (files) {
-          if (files.type.split('/')[0] !== 'image') {
-              this.showMessage(`The file doesn't look like a valid image.`);
-              return;
-          };
-          if (files.size > 10485760) {
-              this.showMessage(`Sorry. The image is too big. Try one below 10 MB.`);
-              return;
-          };
-
-          formRef.current['textInput'].value = `Upload mode selected:\n${files.name}`;
-          formRef.current['textInput'].setAttribute("disabled", "true");
-
-          fileReader.readAsDataURL(files);
-      }
-  }
-
-  onDetect = async (event) => {
-      this.setState({ isLoading: true });
-
-      let url = this.state.imageSourceIsLocal ? this.state.imageUrl.replace(/^data:image.+;base64,/, '') : this.state.imageUrl;
-
-      try
-      {
-          await loadImage(this.state.imageUrl); //loadImage will throw an error if it's not an image
-
-          const regions = await get_from_server('/image/predict', {
-              method: 'post',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({ imageUrl: url })
-          });
-
-          const entries = await get_from_server('/image/afterpredict', {
-              method: 'put',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({ id: this.state.user.id })
-          });
-
-          this.setState(Object.assign(this.state.user, {entries: entries}));
-          this.setState({ isLoading: false });
-
-          this.displayFaceBoxes(this.calculateFacesLocations(regions));
-
-          imageRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
-      }
-      catch (error) {
-          this.showMessage(error);
-          this.setState({ isLoading: false });
-      }
-  }
-
-
-  onClear = () => {
-      formRef.current.reset();
-      formRef.current['textInput'].removeAttribute("disabled");
-      this.setState({ imageUrl: '', imageSourceIsLocal: true, boxes: [], message: '', isLoading: false });
-  }
-
-  showMessage = (message) => {
-      if (typeof message !== 'string') message = 'Unknown error. Try uploading a smaller image.';
-      
-      this.setState({ message: message }, () => {
-          messageRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
-      });
-
-      setTimeout(() => {this.setState({ message: '' })}, 4000);
-  }
-
-  changeToRoute = (route) => {
-      switch(route) {
-        case 'Register':
-            this.setState({ route: route });
-            break;
-        case 'SignIn':
-            this.setState({ route: route });
-            break;
-        case 'SignOut':
-            this.setState(initialState);
-            break;
-        case 'Home':
-            this.setState({ isSignedIn: true, route: route});
-            break;
-        case 'Profile':
-            console.log('If you are seeing this, have an awesome day.')
-            break;
-        default:
-            console.log('Ruta? No hay ruta');
-      }
-  }
-
-
-  render() {
-      const {isSignedIn, route, user, message, imageUrl, isLoading} = this.state;
-
-      return isSignedIn
-      ? (
-        <Fragment>
-            <Particles className='particles' params={particlesOptions} />
-            <Navigation user={user} isSignedIn={isSignedIn} changeToRoute={this.changeToRoute}/>
-            <main className='main-section'>
-                <aside className='sidebar'>
-                    <Logo />
-                    <Rank user={user}/>
-                    <ImageLinkForm
-                      isLoading={isLoading}
-                      onInputChange={this.onInputChange}
-                      onImageUpload={this.onImageUpload}
-                      onDetect={this.onDetect}
-                      onClear={this.onClear}
-                      ref={formRef}
-                    />
-                    <p
-                      style={{fontSize: '1.5rem', color: '#ffbb00', fontWeight:'300'}}
-                      ref={messageRef}>{message}</p>
-                </aside>
-                <section className='section'>
-                    <FaceRecognition
-                      boxes={this.state.boxes}
-                      imageUrl={imageUrl}
-                      ref={imageRef}
-                    />
-                </section>
-            </main>
-        </Fragment>
-      )
-      : (
-        <Fragment>
-            <Particles className='particles' params={particlesOptions} />
-            <Navigation user={user} isSignedIn={isSignedIn} changeToRoute={this.changeToRoute}/>
-            <Logo />
-            {
-              route === 'SignIn'
-              ? <SignIn loadUser={this.loadUser} changeToRoute={this.changeToRoute} />
-              : <Register loadUser={this.loadUser} changeToRoute={this.changeToRoute} />
+    loadUser = user => {
+        this.setState({
+            isSignedIn: true,
+            user: {
+            id:       user.id,
+            name:     user.name,
+            email:    user.email,
+            entries:  user.entries,
+            joined:   user.joined,
             }
-        </Fragment>
-      );
-  }
+        }, () => {})
+    }
+
+    calculateFacesLocations = (clarifai_regions) => {
+        const width = Number(imageRef.current.width);
+        const height = Number(imageRef.current.height);
+
+        const boxes = clarifai_regions.map(region => {
+            let clarifai_box = region.region_info.bounding_box;
+            return {
+              leftCol:    width   * clarifai_box.left_col,
+              topRow:     height  * clarifai_box.top_row,
+              rightCol:   width   - (width  * clarifai_box.right_col),
+              bottomRow:  height  - (height * clarifai_box.bottom_row),
+            }
+        })
+
+        return boxes;
+    }
+
+    displayFaceBoxes = (boxes) => {
+        this.setState({ boxes: boxes });
+    }
+
+    onInputChange = (event) => {
+        this.setState({ imageUrl: event.target.value, imageSourceIsLocal: false, boxes: [] });
+    }
+    
+    onImageUpload = (event) => {
+
+        const files = event.target.files[0];
+
+        let fileReader = new FileReader();
+
+        fileReader.onloadend = async () => {
+            this.setState({ imageUrl: fileReader.result, imageSourceIsLocal: true, boxes: [] });
+        }
+
+        if (files) {
+            if (files.type.split('/')[0] !== 'image') {
+                this.showMessage(`The file doesn't look like a valid image.`);
+                return;
+            };
+            if (files.size > 10485760) {
+                this.showMessage(`Sorry. The image is too big. Try one below 10 MB.`);
+                return;
+            };
+
+            formRef.current['textInput'].value = `Upload mode selected:\n${files.name}`;
+            formRef.current['textInput'].setAttribute("disabled", "true");
+
+            fileReader.readAsDataURL(files);
+        }
+    }
+
+    onDetect = async (event) => {
+        this.setState({ isLoading: true });
+
+        let url = this.state.imageSourceIsLocal ? this.state.imageUrl.replace(/^data:image.+;base64,/, '') : this.state.imageUrl;
+
+        try
+        {
+            await loadImage(this.state.imageUrl); //loadImage will throw an error if it's not an image
+
+            const regions = await get_from_server('/image/predict', {
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ imageUrl: url })
+            });
+
+            const entries = await get_from_server('/image/afterpredict', {
+                method: 'put',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ id: this.state.user.id })
+            });
+
+            this.setState(Object.assign(this.state.user, {entries: entries}));
+            this.setState({ isLoading: false });
+
+            this.displayFaceBoxes(this.calculateFacesLocations(regions));
+
+            imageRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
+        }
+        catch (error) {
+            this.showMessage(error);
+            this.setState({ isLoading: false });
+        }
+    }
+
+
+    onClear = () => {
+        formRef.current.reset();
+        formRef.current['textInput'].removeAttribute("disabled");
+        this.setState({ imageUrl: '', imageSourceIsLocal: true, boxes: [], message: '', isLoading: false });
+    }
+
+    showMessage = (message) => {
+        if (typeof message !== 'string') message = 'Unknown error. Try uploading a smaller image.';
+        
+        this.setState({ message: message }, () => {
+            messageRef.current.scrollIntoView({behavior: "smooth", block: "nearest", inline: "nearest"});
+        });
+
+        setTimeout(() => {this.setState({ message: '' })}, 4000);
+    }
+
+
+    changeToRoute = (route) => {
+        switch(route) {
+            case 'Register':
+                this.setState({ route: route });
+                break;
+            case 'SignIn':
+                this.setState({ route: route });
+                break;
+            case 'SignOut':
+                this.setState(initialState);
+                break;
+            case 'Home':
+                this.setState({ isSignedIn: true, route: route});
+                break;
+            case 'Profile':
+                console.log('If you are seeing this, have an awesome day.')
+                break;
+            default:
+                console.log('Ruta? No hay ruta');
+        }
+    }
+
+
+    render() {
+        const {isSignedIn, route, user, message, imageUrl, isLoading} = this.state;
+
+        return isSignedIn
+        ? (
+            <Fragment>
+                <Particles className='particles' params={particlesOptions} />
+                <Navigation user={user} isSignedIn={isSignedIn} changeToRoute={this.changeToRoute}/>
+                <main className='main-section'>
+                    <aside className='sidebar'>
+                        <Logo />
+                        <Rank user={user}/>
+                        <ImageLinkForm
+                            isLoading={isLoading}
+                            onInputChange={this.onInputChange}
+                            onImageUpload={this.onImageUpload}
+                            onDetect={this.onDetect}
+                            onClear={this.onClear}
+                            ref={formRef}
+                        />
+                        <p
+                            style={{fontSize: '1.5rem', color: '#ffbb00', fontWeight:'300'}}
+                            ref={messageRef}>{message}</p>
+                    </aside>
+                    <section className='section'>
+                        <FaceRecognition
+                            boxes={this.state.boxes}
+                            imageUrl={imageUrl}
+                            ref={imageRef}
+                        />
+                    </section>
+                </main>
+            </Fragment>
+        )
+        : (
+            <Fragment>
+                <Particles className='particles' params={particlesOptions} />
+                <Navigation user={user} isSignedIn={isSignedIn} changeToRoute={this.changeToRoute}/>
+                <Logo />
+                {
+                    route === 'SignIn'
+                        ? <SignIn loadUser={this.loadUser} changeToRoute={this.changeToRoute} />
+                        : <Register loadUser={this.loadUser} changeToRoute={this.changeToRoute} />
+                }
+            </Fragment>
+        );
+    }
 }
 
 export default App;
